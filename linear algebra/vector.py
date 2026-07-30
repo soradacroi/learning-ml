08d2e79a151795895f43385ce896cc47af9e6a91
soradacroi <soradacroi@gmail.com>
2026-07-30 17:30:07 +0530

    Update vector.py

--- linear algebra/vector.py
+++ linear algebra/vector.py
@@ -3,4 +3,8 @@ from typing import List
 
 class vector:
     def __init__(self, vec: List) -> None:
-        pass
+        vec = self.vec
+        n = len(vec)
+
+    def scaler_addition(v, k):
+        return i + k for i in v 
