8914abca07ccf913e07ff73c70523793ec4e50c1
Wali <waliahmedwarsi1@gmail.com>
2026-07-30 18:38:15 +0530

    Update vector.py
    Added dot_product

--- linear algebra/vector.py
+++ linear algebra/vector.py
@@ -8,3 +8,21 @@ class vector:
 
     def scaler_addition(v, k):
         return i + k for i in v 
+
+
+    def dot_product(v,k):
+        if len(v)!=len(k):
+            raise ValueError("Error: Length of the vectors should be the same")
+        dot=0
+        for i in range(len(v)):
+            dor+=v[i]*k[i]
+        return dot
+    def cross_product(v,k):
+        
+            
+        
+
+
+
+
+    
