3d0c9f49768d8b962c12722154db037e7154d7bd
Wali <waliahmedwarsi1@gmail.com>
2026-07-30 19:47:24 +0530

    Update vector.py
    added cross_product

--- linear algebra/vector.py
+++ linear algebra/vector.py
@@ -15,14 +15,23 @@ class vector:
             raise ValueError("Error: Length of the vectors should be the same")
         dot=0
         for i in range(len(v)):
-            dor+=v[i]*k[i]
+            dot+=v[i]*k[i]
         return dot
     def cross_product(v,k):
+        if len(v) !=3 or len(k) !=3 :
+             raise ValueError("Error: The length of the vectors shoule be 3")
+        if  type(v)!=list or type(k)!=list:
+            raise ValueError("Error: The type is not list")
+        prod=[]
+        for i in range(3):
+            if i==0:
+                prod.append((v[1]*k[2])-(v[2]*k[1]))            
         
-            
-        
-
-
+            elif i==1:
+                prod.append((v[2]*k[0])-(v[0]*k[2]))
+            else :
+                 prod.append((v[0]*k[1])-(v[1]*k[0]))
+        return prod
 
 
     
