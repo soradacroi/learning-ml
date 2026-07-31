57ab1bf0c8a2c2003b855596b0ed40cd3c9c5777
soradacroi <soradacroi@gmail.com>
2026-08-01 00:22:48 +0530

    Refactor Vector class and implement methods
    Refactor vector class and methods for clarity and functionality.

--- linear algebra/vector.py
+++ linear algebra/vector.py
@@ -1,18 +1,24 @@
 from typing import List
 
+VectorType = List[float]
 
-class vector:
-    def __init__(self, vec: List) -> None:
-        vec = self.vec
-        n = len(vec)
+class Vector:
+    def __init__(self, vec: VectorType) -> None:
+        self.vec = vec
+        self.n = len(self.vec)
 
-    def scaler_addition(v, k):
-        return i + k for i in v 
+    def scaler_addition(self, k: int | float) -> VectorType:
+        """adds a scalar value `k` to each
+        of the element of the vector"""
+        return [i + k for i in self.vec]
 
-    def dot_product(v: List, w: List) -> List:
-        assert len(v) == len(w), "vectors should be in same length"
-        
-        return sum([i * j for i, j in zip(v, w)])
+    def dot_product(self, w: VectorType) -> float:
+        """calculates the dot product
+        `self.vec` and vector `w`"""
+        assert len(self.vec) == len(w), "vectors should be in same length"
+        return sum([i * j for i, j in zip(self.vec, w)])
+
+    """
         
     def cross_product(v,k):
         if len(v) !=3 or len(k) !=3 :
@@ -29,6 +35,14 @@ class vector:
             else :
                  prod.append((v[0]*k[1])-(v[1]*k[0]))
         return prod
+    """
+
+# btw @wali we were doing it wrong so like i was not really like thinking while i started making it
+# so can u see the formate i wrote it rn, like read it carefully how i am using self and vec and 
+# how i am making the function, dont wory about the types (that is: VectorType and like hintings)
+# and dont wory about the docstrings
+# try to like refactor the `cross_produckt()` like how i did uppar pe 
+# thankyou and sorry
 
 
     
